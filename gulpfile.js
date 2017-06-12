9730db7d834a1ed0ee6c5ac9f3a1447bf4e5574a
David Gusel <david@gusel.org>
2017-06-13 01:11:02 +0200

    build system started

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create(),
+reload = browserSync.reload,
+autoprefixer = require('gulp-autoprefixer'),
+sass = require('gulp-sass'),
+useref = require('gulp-useref'),
+uglify = require('gulp-uglify'),
+gulpIf = require('gulp-if'),
+cssnano = require('gulp-cssnano'),
+imagemin = require('gulp-imagemin'),
+cache = require('gulp-cache'),
+del = require('del'),
+runSequence = require('run-sequence');
+
+// Default Task
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+
+    gulp.watch('app/assets/styles/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/assets/scripts/**/*.js', browserSync.reload); 
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('app/assets/styles/**/*.scss')
+        .pipe(sass({     
+            includePaths: require('node-normalize-scss').includePaths
+        }))
+        // Prevents watch task from quitting on CSS error
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+         .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/assets/temp/styles'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+// Gulp Build Task
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('/assets/temp/styles/*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/assets/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/assets/fonts/**/*')
+  .pipe(gulp.dest('dist/assets/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
