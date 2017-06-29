e5de2f966affb57e882f3d969956b528dcc33c94
David Gusel <david@gusel.org>
2017-06-29 15:25:38 +0200

    added fading to mobile menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -5,11 +5,13 @@ class MobileMenu {
 	constructor() {
 		this.menuIcon = $('.menu-icon');
 		this.menuContent = $('.main-navigation');
+		this.menuListItem = $('.main-navigation li');
 		this.events();
 	}
 	// Handle Click Events
 	events() {
 		this.menuIcon.click(this.toggleTheMenu.bind(this));
+		this.menuListItem.click(this.toggleTheMenu.bind(this));
 	}
 	// Set Behaviour
 	toggleTheMenu() {
