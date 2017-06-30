3232d9ca703b98a122788429ca4aebb2baf175c1
David <dvdgsl@gmail.com>
2017-06-30 20:16:44 +0200

    implemented smooth scrolling and lazy loading images

--- app/assets/scripts/modules/CopyEmail.js
+++ app/assets/scripts/modules/CopyEmail.js
@@ -3,16 +3,24 @@ import $ from 'jquery';
 class CopyEmail {
 	constructor() {
 		this.copyEmail = $('.copy-email');
+		this.mailTo = $('.mailto');
 		this.copyEmailNotification = $('.copy-email-notification');
 		this.events();
 	}
 
 	events() {
 		this.copyEmail.click(this.copyToClipboard.bind(this));
+		this.mailTo.click(this.clickMailTo());
 	}
 
 	copyToClipboard() {
+		console.log('clicked');
 		this.copyEmailNotification.toggleClass('copy-email-notification--visible');
+	}
+
+	clickMailTo() {
+		console.log('clicked2');
+		// this.copyEmailNotification.textContent('Opening your email client');
 	}
 }
 
