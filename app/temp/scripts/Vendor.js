10e47189b813b3626229ad13574198d0a50aa7ba
David Gusel <david@gusel.org>
2018-07-12 09:47:40 +0200

    fix: make menu visible on desktop again

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 8);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 6:
+/***/ 4:
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -773,13 +773,13 @@
 
 /***/ }),
 
-/***/ 8:
+/***/ 6:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(6);
+__webpack_require__(4);
 
 /***/ })
 
