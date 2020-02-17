a1d2df18db9a2e27278a66c77478dbe081a57b8f
David Gusel <david.gusel@restplatzboerse.at>
2020-02-17 08:16:05 +0100

    chore: remove gulp

--- gulp/tasks/default.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-runSequence = require('run-sequence');
-
-// Default Task
-gulp.task('default', function (callback) {
-  runSequence(['sass', 'scripts', 'watch'],
-    callback
-  )
-});
--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-require('./gulp/tasks/default');
-require('./gulp/tasks/sass');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/modernizr');
-require('./gulp/tasks/scripts');
-require('./gulp/tasks/build');
