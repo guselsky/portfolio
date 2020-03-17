5eda7ae823638f89c2c95cb6ba077a188f0e5031
David Gusel <david.gusel@restplatzboerse.at>
2020-03-17 08:25:02 +0100

    chore: replace gulp with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production' ? true : false;
 
 module.exports = {
-	entry: {
-		App: path.resolve(__dirname, "./app/assets/scripts/App.js"),
-		Vendor: path.resolve(__dirname, "./app/assets/scripts/Vendor.js")
-	},
-	output: {
-		path: path.resolve(__dirname, "./app/temp/scripts"),
-		filename: "[name].js"
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015']
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
-			}
-		]
-	}
-}
+  entry: './src/scripts/App.js',
+  output: {
+    filename: 'main.js',
+    path: isProduction ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, './'),
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./**/*.html')
+    },
+    contentBase: path.join(__dirname, './'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ],
+  },
+};
