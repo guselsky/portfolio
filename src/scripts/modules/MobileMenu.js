a94d979c885bc9b59ad55118c3f9d94ddc50e7a5
David Gusel <david.gusel@restplatzboerse.at>
2020-04-10 16:44:14 +0200

    refactor: optimize header.

--- src/scripts/modules/MobileMenu.js
+++ src/scripts/modules/MobileMenu.js
@@ -1,21 +1,18 @@
-import $ from 'jquery';
-
 class MobileMenu {
-	// Select From The DOM
 	constructor() {
-		this.menuIcon = $('.menu-icon');
-		this.menuContent = $('.header-navigation');
-		this.menuListItem = $('.header-navigation li');
+		this.menuIcon = document.querySelector('.menu-icon');
+		this.menuContent = document.querySelector('.header-menu');
+		this.menuListItem = document.querySelector('.header-menu li');
 		this.events();
 	}
-	// Handle Click Events
+
 	events() {
-		this.menuIcon.click(this.toggleTheMenu.bind(this));
-		this.menuListItem.click(this.toggleTheMenu.bind(this));
+		this.menuIcon.addEventListener('click', this.toggleTheMenu.bind(this));
+		this.menuListItem.addEventListener('click', this.toggleTheMenu.bind(this));
 	}
-	// Set Behaviour
+
 	toggleTheMenu() {
-		this.menuContent.toggleClass('header-navigation--visible');
+		this.menuContent.classList.toggle('header-menu--visible');
 	}
 }
 
