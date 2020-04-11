d46b6b4ced71329c6dc64eeb77af616f0af1d540
David Gusel <david.gusel@restplatzboerse.at>
2020-04-11 18:52:52 +0200

    clean: remove unused imports.

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,12 +1,3 @@
 import MobileMenu from './modules/MobileMenu';
-// import RevealOnScroll from './modules/RevealOnScroll'
-// import $ from 'jquery';
-import SmoothScroll from './modules/SmoothScroll';
 
 var mobileMenu = new MobileMenu();
-var copyEmail = new CopyEmail();
-new Clipboard('.copy-email');
-// new RevealOnScroll($(".portfolio__item"), "85%");
-// new RevealOnScroll($(".testimonials__testimonial"), "90%");
-// new RevealOnScroll($(".about-me"), "90%");
-var smoothScroll = new SmoothScroll();
