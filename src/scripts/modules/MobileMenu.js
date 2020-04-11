4e46c0634ce856627c632d8bcefa706ee680fa3b
David Gusel <david@gusel.org>
2020-04-11 15:40:43 +0200

    fix: mobile menu closes on item click.

--- src/scripts/modules/MobileMenu.js
+++ src/scripts/modules/MobileMenu.js
@@ -1,14 +1,16 @@
 class MobileMenu {
 	constructor() {
-		this.menuIcon = document.querySelector('.menu-icon');
-		this.menuContent = document.querySelector('.header-menu');
-		this.menuListItem = document.querySelector('.header-menu li');
+		this.menuIcon = document.getElementsByClassName('menu-icon')[0];
+		this.menuContent = document.getElementsByClassName('header-menu')[0];
+		this.menuListItems = document.getElementsByClassName('header-menu__list-item');
 		this.events();
 	}
 
 	events() {
 		this.menuIcon.addEventListener('click', this.toggleTheMenu.bind(this));
-		this.menuListItem.addEventListener('click', this.toggleTheMenu.bind(this));
+		for (let i = 0; i < this.menuListItems.length; i++) {
+			this.menuListItems[i].addEventListener('click', this.toggleTheMenu.bind(this));
+		}
 	}
 
 	toggleTheMenu() {
