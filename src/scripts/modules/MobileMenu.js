368d2349f23e9a16de357637a599e4b422a9ca66
David Gusel <david@gusel.org>
2020-04-16 16:33:11 +0200

    feat: close mobile menu on logo click.

--- src/scripts/modules/MobileMenu.js
+++ src/scripts/modules/MobileMenu.js
@@ -3,6 +3,7 @@ class MobileMenu {
 		this.menuIcon = document.getElementsByClassName('menu-icon')[0];
 		this.menuContent = document.getElementsByClassName('header-menu')[0];
 		this.menuListItems = document.getElementsByClassName('header-menu__list-item');
+		this.logo = document.getElementsByClassName('logo')[0];
 		this.events();
 	}
 
@@ -11,10 +12,15 @@ class MobileMenu {
 		for (let i = 0; i < this.menuListItems.length; i++) {
 			this.menuListItems[i].addEventListener('click', this.toggleTheMenu.bind(this));
 		}
+		this.logo.addEventListener('click', this.closeTheMenu.bind(this));
 	}
 
 	toggleTheMenu() {
 		this.menuContent.classList.toggle('header-menu--visible');
+	}
+
+	closeTheMenu() {
+		this.menuContent.classList.remove('header-menu--visible');
 	}
 }
 
