d7e97c2eee88283b8634c6aa4f19bde6c30d3c1a
David Gusel <david@gusel.org>
2026-02-22 13:18:01 +0100

    make mobile menu a dialog

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,33 @@
 (function () {
-    const menuButton = document.getElementsByClassName('menu-button')[0];
-    const menuContent = document.getElementsByClassName('header-menu')[0];
-    const menuListItems = document.getElementsByClassName('header-menu__list-item');
-    const header = document.getElementsByClassName('header-container')[0];
+  const menuButton = document.getElementsByClassName("menu-button")[0];
+  const menuListItems = document.getElementsByClassName(
+    "header-menu__list-item",
+  );
+  const header = document.getElementsByClassName("header-container")[0];
+  const mobileMenu = header.getElementsByTagName("dialog")[0];
 
-    let menuIsOpen = false;
+  let menuIsOpen = false;
 
-    const toggleMenu = (e) => {
-        menuContent.classList.toggle('header-menu--visible');
-        menuIsOpen = !menuIsOpen;
-        menuButton.setAttribute('aria-expanded', String(menuIsOpen));
+  const openMenu = (e) => {
+    menuIsOpen = true;
+    menuButton.setAttribute("aria-expanded", String(menuIsOpen));
+    mobileMenu.showModal();
+  };
+
+  const closeMenu = (e) => {
+    if (e.target === menuButton) {
+      return false;
     }
+    menuIsOpen = false;
+    menuButton.setAttribute("aria-expanded", String(menuIsOpen));
+    mobileMenu.close();
+  };
 
-    const closeMenu = (e) => {
-        if (e.target === menuButton) {
-            return false;
-        }
-        menuIsOpen = false;
-        menuContent.classList.remove('header-menu--visible');
-    }
+  menuButton.addEventListener("click", openMenu);
 
-    menuButton.addEventListener('click', toggleMenu);
+  for (let i = 0; i < menuListItems.length; i++) {
+    menuListItems[i].addEventListener("click", closeMenu);
+  }
 
-    for (let i = 0; i < menuListItems.length; i++) {
-        menuListItems[i].addEventListener('click', toggleMenu);
-    }
-
-    header.addEventListener('click', closeMenu);
-})()
+  header.addEventListener("click", closeMenu);
+})();
